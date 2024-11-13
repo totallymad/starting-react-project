2185f3213eb1e480614c126fb1ee8b0f4e5c38e4
Ilya <ilya02989@gmail.com>
2024-11-13 16:38:10 +0300

    Passing Functions as Values to Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data';
-
-const reactDesctrictions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function MainGoal() {
-  return (
-    <p>"My main goal: Learn React in-depth and from the ground up"</p>
-  )
-}
-
-function Header() {
-  const description = reactDesctrictions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-}
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
+  function handleSelect() {
+    console.log('Hello world - selected!')
+  }
+
   return (
     <div>
       <Header />
@@ -53,7 +24,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <MainGoal />
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+          Dynamic Content
+        </section>
       </main>
     </div>
   );
