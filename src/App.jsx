18a4188e4e1931ff3a5ea919f5a369b0485c782f
Ilya <ilya02989@gmail.com>
2024-11-14 11:00:44 +0300

    Conditional Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from './components/TabButton';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  const [del, setDel] = useState(false);
+
+  function handleDelete() {
+    setDel(!del);
+    console.log('deleted');
+    console.log(del)
+  }
+
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton)
+    // console.log(selectedTopic);
+
+  }
+
+
+  let delItem = <button onClick={handleDelete} id='delete'>Delete</button>
+
+  if (del) {
+    delItem = (
+      <div id="alert">
+        <h3>Are you sure?</h3>
+        <p>These changes cant't be reverted</p>
+        <button onClick={handleDelete}>Procceed </button>
+      </div>
+    )
+  }
+
+
+  // console.log('APP COMPONENT RENDERING');
+
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (<div id='tab-content'>
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>)
   }
 
   return (
@@ -32,7 +76,31 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {/* {!selectedTopic ? <p>Please select a topic.</p> : null}
+          {selectedTopic ? (<div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>) : null} */}
+          {/* {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (<div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>)} */}
+          {tabContent}
+
+        </section>
+        <section>
+          {delItem}
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,4 +1,6 @@
 export default function TabButton({ children, onSelect }) {
+  // console.log('TAB-BUTTON COMPONENT RENDERING')
+
     return (
     <li>
         <button onClick={onSelect}>{children}</button>
